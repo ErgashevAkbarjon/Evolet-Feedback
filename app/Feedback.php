fee9bbbb44d0279458ba093cc02501865cab3abf
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-17 15:21:17 +0500

    Define explisiply table name for Feedback model

--- app/Feedback.php
+++ app/Feedback.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
+    protected $table = "feedbacks";
+
     public function group()
     {
         return $this->belongsTo(Group::class);
