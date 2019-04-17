1a67afc65f881ae80c7fc20c48b11db98b68e9c5
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-17 15:23:39 +0500

    Define model factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,19 +1,54 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'full_name' => $faker->name,
         'email' => $faker->email,
+        'password' => $faker->password
     ];
 });
+
+$factory->define(App\Customer::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => randomIdFrom(App\User::class),
+        'pc_id' => randomIdFrom(App\PC::class),
+        'bonus' => '0',
+    ];
+});
+
+$factory->define(App\Employee::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => randomIdFrom(App\User::class),
+        'avatar' => $faker->imageUrl(200,200,'cats'),
+    ];
+});
+
+$factory->define(App\Feedback::class, function (Faker\Generator $faker) {
+    return [
+        'group_id' => randomIdFrom(App\Group::class),
+        'status_id' => randomIdFrom(App\Status::class),
+        'type_id' => randomIdFrom(App\Group::class),
+        'customer_id' => randomIdFrom(App\Customer::class),
+        'description' => $faker->text,
+    ];
+});
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'body' => $faker->text,
+        'employee_id' => randomIdFrom(App\Employee::class)
+    ];
+});
+
+/**
+ * Helpers
+ */
+
+function randomIdFrom($class)
+{
+    $id = $class::inRandomOrder()->first()->id;
+    if ($id === null) {
+        $id = 0;
+    }
+    return $id;
+}
