87e53594345fdb64b428fbad66d957b918ff47eb
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-18 15:34:01 +0500

    Implement update() method

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Feedback;
+use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
@@ -30,5 +31,30 @@ class FeedbackController extends Controller
         ])->find($id);
 
         return $this->jsonUtf($result);
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function update($id, Request $request)
+    {
+        $feedback = Feedback::find($id);
+        $requests = $request->all();
+
+        $modelProps = array_keys($feedback->getAttributes());
+
+        foreach ($requests as $key => $value) {
+            if(in_array($key, $modelProps)){
+                $feedback->$key = $value;
+            }
+        }
+
+        $feedback->save();
+    }
+
+    public function destroy($id)
+    {
+
     }
 }
