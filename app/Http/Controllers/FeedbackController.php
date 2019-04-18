3122a28f243ab8c9adebcdcbaf2aabc04b9b9932
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-18 12:03:47 +0500

    Add "index" and "show" methods to Feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Feedback;
+
 class FeedbackController extends Controller
 {
     public function index()
     {
-        
+        $result = Feedback::with([
+                'status',
+                'customer.user:id,full_name',
+                'customer.pc:id,name'
+            ])->get();
+
+        return $this->jsonUtf($result);
+
+    }
+
+    public function show($id)
+    {
+        $result = Feedback::with([
+            'status',
+            'group',
+            'type',
+            'customer.user:id,full_name',
+            'customer.pc:id,name',
+            'files'
+        ])->find($id);
+
+        return $this->jsonUtf($result);
     }
 }
