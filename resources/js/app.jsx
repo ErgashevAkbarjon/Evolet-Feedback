e08e08a3ffa182cbd4aeb9c9903f3e63a2e60bc5
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-26 09:49:18 +0500

    Finish React set up

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,16 @@
+require('./bootstrap');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Main from './views/Main';
 
 function App(props) {
     return (
-        <div>
-            "slkdcnlskdcnlksnc"
-        </div>
+        <BrowserRouter>
+            <Route path='/' component={Main}/>
+        </BrowserRouter>
     );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
