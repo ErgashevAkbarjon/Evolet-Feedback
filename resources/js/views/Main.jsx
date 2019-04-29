ff719b0547821db3c3a3f30f05a122e3473a5353
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-29 20:13:43 +0500

    Add app style scaffolding

--- resources/js/views/Main.jsx
+++ resources/js/views/Main.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import { 
+    Dropdown,
+    Container,
+    Row,
+    Col
+ } from "react-bootstrap";
+import withStyles from 'react-jss';
 
-export default function Main(props) {
-    return(
-        <div>
+const styles = {
+    main:{
+        fontSize: '22px',
+        fontFamily: 'Segoe UI'
+    },
+    sidebar: {
+        background: "#253338",
+        minHeight: "100vh",
+        color: 'white',
+        fontSize: '1.25rem'
+    },
+    
+}
 
-        </div>
+function Main(props) {
+    const {classes} = props;
+    return (
+        <Container fluid className={classes.main}>
+            <Row>
+                <Col xs={1} className={classes.sidebar}>
+                    <h1>Feedback</h1>
+                    
+                    <nav>Упаковки</nav>
+                    <nav>Упаковки</nav>
+                    <nav>Упаковки</nav>
+                </Col>
+                <Col>
+
+                </Col>         
+            </Row>
+        </Container>
     );
 }
+
+export default withStyles(styles)(Main);
