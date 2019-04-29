a21596bbb66770dce50e633db82976dc99dab06b
Ergashev Akbar <ergashev.akb@gmail.com>
2019-04-29 20:10:03 +0500

    Add Bootatrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Evolet Feedback</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/css/app.css"/>
 </head>
 <body>
     <div id="root"></div>
