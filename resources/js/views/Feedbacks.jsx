e9d006f1996bf25fb8f3513ae605e4d1ad464611
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-01 17:38:43 +0500

    Add Feedbacks component

---
+++ resources/js/views/Feedbacks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import withStyles from 'react-jss';
+
+const styles = {
+
+};
+
+function Feedbacks({classes}) {
+    return (
+        <div>
+            SomeS
+        </div>
+    );
+}
+
+export default withStyles(styles)(Feedbacks);
