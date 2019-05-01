215d91cfbd18cce68d338b762f4823bdd97e5b30
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-01 17:35:29 +0500

    Add routes

---
+++ resources/js/routes.js
@@ -0,0 +1,13 @@
+
+import Feedbacks from './views/Feedbacks';
+
+export default [
+    {
+        path: '/feedbacks',
+        component: Feedbacks
+    },
+]
+
+export const SidebarGroups = {
+    path: '/api/groups'
+}
