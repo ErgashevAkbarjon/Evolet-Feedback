7ee526eb2c7fd349e02f4032dd61e0707c6bcc5b
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-01 17:36:13 +0500

    Make Sidebar as a separate component

---
+++ resources/js/components/Sidebar.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import withStyles from 'react-jss';
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { SidebarGroups } from '../routes';
+import { NavLink } from 'react-router-dom';
+
+const styles = {
+    sidebar: {
+        background: "#253338",
+        minHeight: "100vh",
+        width: '100%',
+        color: 'white',
+        fontSize: '1.3rem',
+    },
+    sidebarLink: {
+        paddingLeft: '24px',
+        color: 'white',
+        width: '100%',
+        margin: '6px 0px',
+        display: 'inherit',
+        '&:hover': {
+            color: 'white',
+            textDecoration: 'none',
+        }
+    },
+    sidebarLinkActive: {
+        background: '#f3f3f340',
+    },
+    header: {
+        fontSize: '2rem',
+        padding: '24px',
+        fontWeight: '400',
+    },
+}
+
+function Sidebar({ classes }) {
+
+    const [groups, setGroups] = useState();
+    
+    useEffect(() => {
+        axios
+            .get(SidebarGroups.path)
+            .then(({ data }) => setGroups(data))
+            .catch((e) => { console.log(e) });
+    }, []);
+
+    return (
+        <Row>
+            <div className={classes.sidebar}>
+                <h1 className={classes.header}>
+                    Feedback
+                </h1>
+
+                <nav >
+                    {
+                        groups ?
+                            groups.map((group, i) => (
+                                <div key={i}>
+                                    <NavLink
+                                        to='/feedbacks'
+                                        className={classes.sidebarLink}
+                                        activeClassName={classes.sidebarLinkActive}
+                                    >
+                                        {group.name}
+                                    </NavLink>
+                                </div>
+                            ))
+                            :
+                            (<p>Загрузка...</p>)
+                    }
+                </nav>
+            </div >
+        </Row>
+    );
+}
+
+export default withStyles(styles)(Sidebar);
