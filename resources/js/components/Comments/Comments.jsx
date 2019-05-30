8ba215282c1f2f09bd4b1847ff28aa7cbc437869
Ergashev Akbar <ergashev.akb@gmail.com>
2019-05-30 10:26:37 +0500

    Convert coment input to a component

--- resources/js/components/Comments/Comments.jsx
+++ resources/js/components/Comments/Comments.jsx
@@ -1,20 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import withStyles from 'react-jss'
 import { isArray } from 'util';
 
 import Comment from './Comment';
+import CommentInput from './CommentInput';
 
 const styles = {
-    commentInput: {
-        borderColor: '#707070',
-    },
-    commentInputWrapper: {
-        textAlign: 'end',
-    },
-    commentButton: {
-        borderRadius: '50px',
-        padding: '.5em 1.8em',
-    },
     commentsHead: {
         color: '#707070',
         fontWeight: '400'
@@ -22,46 +13,22 @@ const styles = {
 }
 
 function Comments({ classes, comments, addCallback }) {
-    const [newComment, setNewComment] = useState();
-
-    const addComment = () => {
-        if(newComment.lenght < 2) return;
-
-        addCallback(newComment);
-        setNewComment('');
-    }
-
     return (
-        <React.Fragment>
-            <div className={classes.commentInputWrapper}>
-                <textarea
-                    className={'form-control ' + classes.commentInput}
-                    rows="4"
-                    placeholder='Напишите коментарий...'
-                    value={newComment}
-                    onChange={(e) => setNewComment(e.target.value)}
-                />
-                <button
-                    type="button"
-                    className={'btn btn-primary mt-3 ' + classes.commentButton}
-                    onClick={() => addComment()}
-                >
-                    Отправить
-                </button>
-            </div>
+        <div>
+            <CommentInput addCallback={addCallback} />
             <div>
                 <h3 className={'mb-4 ' + classes.commentsHead}>Обсуждение</h3>
                 <ul className="list-unstyled">
                     {
                         isArray(comments)
                             ? comments.map(comment => (
-                                <Comment comment={comment} key={comment.id} />
+                                <Comment comment={comment} key={comment.id} addCallback={addCallback}/>
                             ))
                             : null
                     }
                 </ul>
             </div>
-        </React.Fragment>
+        </div>
     )
 }
 export default withStyles(styles)(Comments);
