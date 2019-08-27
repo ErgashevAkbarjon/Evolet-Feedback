b60da49dee2bf3d6c8cba3b73d81da71b74859cf
Ergashev Akbar <ergashev.akb@gmail.com>
2019-08-27 14:52:16 +0500

    Refactor

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -1,76 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import withStyles from 'react-jss';
-import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
-import { SidebarGroups } from '../routes';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import withStyles from "react-jss";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import { SidebarGroups } from "../routes";
+import { NavLink } from "react-router-dom";
 
 const styles = {
     sidebar: {
         background: "#253338",
         minHeight: "100vh",
-        width: '100%',
-        color: 'white',
-        fontSize: '1.3rem',
+        width: "100%",
+        color: "white",
+        fontSize: "1.3rem"
     },
     sidebarLink: {
-        paddingLeft: '24px',
-        color: 'white',
-        width: '100%',
-        margin: '6px 0px',
-        display: 'inherit',
-        '&:hover': {
-            color: 'white',
-            textDecoration: 'none',
+        paddingLeft: "24px",
+        color: "white",
+        width: "100%",
+        margin: "6px 0px",
+        display: "inherit",
+        "&:hover": {
+            color: "white",
+            textDecoration: "none"
         }
     },
     sidebarLinkActive: {
-        background: '#f3f3f340',
+        background: "#f3f3f340"
     },
     header: {
-        fontSize: '2rem',
-        padding: '24px',
-        fontWeight: '400',
-    },
-}
+        fontSize: "2rem",
+        padding: "24px",
+        fontWeight: "400"
+    }
+};
 
 function Sidebar({ classes }) {
-
     const [groups, setGroups] = useState();
-    
+
     useEffect(() => {
         axios
             .get(SidebarGroups.path)
             .then(({ data }) => setGroups(data))
-            .catch((e) => { console.log(e) });
+            .catch(e => {
+                console.log(e);
+            });
     }, []);
 
     return (
         <Row>
             <div className={classes.sidebar}>
-                <h1 className={classes.header}>
-                    Feedback
-                </h1>
+                <h1 className={classes.header}>Feedback</h1>
 
-                <nav >
-                    {
-                        groups ?
-                            groups.map((group,  i) => (
-                                <div key={i}>
-                                    <NavLink
-                                        to={'/feedbacks/group/' + group.id}
-                                        className={classes.sidebarLink}
-                                        activeClassName={classes.sidebarLinkActive}
-                                    >
-                                        {group.name}
-                                    </NavLink>
-                                </div>
-                            ))
-                            :
-                            (<p>Загрузка...</p>)
-                    }
+                <nav>
+                    {groups ? (
+                        groups.map((group, i) => (
+                            <div key={i}>
+                                <NavLink
+                                    to={"/feedbacks/group/" + group.id}
+                                    className={classes.sidebarLink}
+                                    activeClassName={classes.sidebarLinkActive}
+                                >
+                                    {group.name}
+                                </NavLink>
+                            </div>
+                        ))
+                    ) : (
+                        <p>Загрузка...</p>
+                    )}
                 </nav>
-            </div >
+            </div>
         </Row>
     );
 }
--- resources/js/views/Feedbacks.jsx
+++ resources/js/views/Feedbacks.jsx
@@ -1,48 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import withStyles from 'react-jss';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import withStyles from "react-jss";
+import axios from "axios";
 
-import Table from '../components/table/Table';
-import FeedbackRow from '../components/FeedbackRow';
+import Table from "../components/table/Table";
+import FeedbackRow from "../components/FeedbackRow";
 
 const styles = {
     title: {
-        color: '#707070',
-        fontWeight: '400'
+        color: "#707070",
+        fontWeight: "400"
     },
     menuWrapper: {
-        width: '10%',
-        flexWrap: 'nowrap',
-        fontSize: '22px',
-        color: '#707070',
-        marginBottom: '.6em',
-        paddingLeft: '15px',
+        width: "10%",
+        flexWrap: "nowrap",
+        fontSize: "22px",
+        color: "#707070",
+        marginBottom: ".6em",
+        paddingLeft: "15px"
     },
     menu: {
-        cursor: 'pointer',
-        paddingBottom: ' 6px',
+        cursor: "pointer",
+        paddingBottom: " 6px"
     },
     menuActive: {
-        borderBottom: '2px solid',
-        borderColor: '#3C95D1',
-        color: '#3C95D1',
+        borderBottom: "2px solid",
+        borderColor: "#3C95D1",
+        color: "#3C95D1"
     }
-
 };
 
-
 const printable = {
-    description: 'Отрывок описания',
-    created_at: 'Дата',
-    "customer.pc": 'ПК',
-    "customer.user.full_name": 'Отправитель',
-    "status.name": 'Статус',
-}
+    description: "Отрывок описания",
+    created_at: "Дата",
+    "customer.pc": "ПК",
+    "customer.user.full_name": "Отправитель",
+    "status.name": "Статус"
+};
 
 function Feedbacks({ classes, match }) {
-
     const [feedbacks, setFeedbacks] = useState();
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState("");
     const [feedbacksType, setType] = useState(1);
 
     const groupId = match.params.id;
@@ -53,48 +50,59 @@ function Feedbacks({ classes, match }) {
         axios
             .get(filteredFeedbacksURL)
             .then(({ data }) => setFeedbacks(data))
-            .catch((e) => console.log(e));
+            .catch(e => console.log(e));
         axios
-            .get('/api/groups?id=' + groupId)
+            .get("/api/groups?id=" + groupId)
             .then(({ data }) => setTitle(data[0].name))
-            .catch((e) => console.log(e));
-
+            .catch(e => console.log(e));
     }, [filteredFeedbacksURL]);
 
     return (
-        <div className='row'>
+        <div className="row">
             <div className="col">
                 <div className={"row " + classes.menuWrapper}>
-                    <div className={`col ${classes.menu} ${feedbacksType === 1 ? classes.menuActive : ''}`} onClick={() => setType(1)}>
+                    <div
+                        className={`col ${classes.menu} ${
+                            feedbacksType === 1 ? classes.menuActive : ""
+                        }`}
+                        onClick={() => setType(1)}
+                    >
                         Проблемы
                     </div>
-                    <div className={`col ${classes.menu} ${feedbacksType === 2 ? classes.menuActive : ''}`} onClick={() => setType(2)}>
+                    <div
+                        className={`col ${classes.menu} ${
+                            feedbacksType === 2 ? classes.menuActive : ""
+                        }`}
+                        onClick={() => setType(2)}
+                    >
                         Идеи
                     </div>
                 </div>
                 <h2 className={classes.title}>{title}</h2>
-                {
-                    feedbacks
-                        ? (<Table>
-                            <thead>
-                                <tr>
-                                    {
-                                        Object.values(printable).map((fieldName, i) => (
-                                            <th key={i}>{fieldName}</th>
-                                        ))
-                                    }
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    feedbacks.map((feedback, i) => (
-                                        <FeedbackRow key={i} feedback={feedback} printableFields={Object.keys(printable)} />
-                                    ))
-                                }
-                            </tbody>
-                        </Table>)
-                        : ("Загрузка...")
-                }
+                {feedbacks ? (
+                    <Table>
+                        <thead>
+                            <tr>
+                                {Object.values(printable).map(
+                                    (fieldName, i) => (
+                                        <th key={i}>{fieldName}</th>
+                                    )
+                                )}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {feedbacks.map((feedback, i) => (
+                                <FeedbackRow
+                                    key={i}
+                                    feedback={feedback}
+                                    printableFields={Object.keys(printable)}
+                                />
+                            ))}
+                        </tbody>
+                    </Table>
+                ) : (
+                    "Загрузка..."
+                )}
             </div>
         </div>
     );
