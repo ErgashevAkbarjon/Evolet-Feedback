297edf00a77e07b11f98842be1dd6a45b842aa42
Ergashev Akbar <ergashev.akb@gmail.com>
2019-11-27 15:13:52 +0500

    Separate customer and employee main page

--- resources/js/views/Main.jsx
+++ resources/js/views/Main.jsx
@@ -3,20 +3,29 @@ import { Redirect } from 'react-router-dom';
 
 import AuthContext from "../components/AuthContext";
 import EmployeeMain from '../views/employee/Main';
+import CustomerMain from '../views/customer/Main';
 
 function Main(props) {
-    const { classes, location } = props;
+    const { location } = props;
     const authContext = useContext(AuthContext);
 
-    const jwtPayload = parseJwt()
+    if(!authContext.auth){
+        return (
+            <Redirect
+                to={{ pathname: "/login", state: { backUrl: location.pathname } }}
+            />
+        );
+    }
 
-    return authContext.auth ? (
-        <EmployeeMain />
-    ) : (
-        <Redirect
-            to={{ pathname: "/login", state: { backUrl: location.pathname } }}
-        />
-    );
+    const jwtPayload = parseJwt(authContext.auth);
+
+    const user = jwtPayload.sub[0];
+
+    if(user.hasOwnProperty('type') && user.type === 1){
+        return (<EmployeeMain />);
+    }
+    
+    return (<CustomerMain />);
 }
 
 export default Main;
