6d7199938ee57bc33aa621dd41ab018513c49d2f
Ergashev Akbar <ergashev.akb@gmail.com>
2019-11-28 18:02:10 +0500

    Add navbar

--- resources/js/views/customer/Main.jsx
+++ resources/js/views/customer/Main.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import { CustomerRoutes } from '../../routes';
+import { CustomerRoutes } from "../../routes";
+import Navbar from "../customer/Navbar";
 
 function Main() {
     return (
-        <div className="container-fluid h-100">
-            <div className="row d-flex justify-content-center align-content-center h-100">
-                <Switch>
-                    {
-                        CustomerRoutes.map((routeProps, i) => (
-                            <Route key={i} {...routeProps}/>
-                        ))
-                    }
-                </Switch>
+        <div className='h-100'>
+            <Navbar />
+            <div className="container-fluid h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <Switch>
+                        {CustomerRoutes.map((routeProps, i) => (
+                            <Route key={i} {...routeProps} />
+                        ))}
+                    </Switch>
+                </div>
             </div>
         </div>
     );
