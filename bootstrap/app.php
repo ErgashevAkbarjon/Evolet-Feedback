df495e19cad08be62412a9c01200a97acffd4598
Ergashev Akbar <ergashev.akb@gmail.com>
2019-12-12 17:35:13 +0500

    Add mail configs to project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,16 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
+
+
+/**
+ * Mail
+ */
+$app->configure('mail');
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 
 /*
 |--------------------------------------------------------------------------
