1f0c91fa024b7d71b9cd7e01b362f01da48fe4f5
Ergashev Akbar <ergashev.akb@gmail.com>
2019-12-17 17:20:38 +0500

    Refactor customers form

--- resources/js/views/employee/customers/NewCustomerModal.jsx
+++ resources/js/views/employee/customers/NewCustomerModal.jsx
@@ -1,77 +1,15 @@
 import React, { useState, useEffect } from "react";
-import withStyles from "react-jss";
 import axios from "axios";
 
 import { ApiRoutes } from "../../../routes";
 import Loading from "../../../components/Loading";
 import CardModal from "../../../components/CardModal";
+import CustomerForm from "../../../components/forms/CustomerForm";
 
-const styles = {
-    actionButton: {
-        borderRadius: "50px",
-        padding: ".5em",
-        color: "white",
-        display: "block",
-        width: "100%",
-        cursor: "pointer",
-        "&:hover": {
-            color: "white"
-        }
-    },
-    acceptActionButton: {
-        background: "#30D92A",
-        borderColor: "#30D92A",
-        "&:focus": {
-            boxShadow: "0 0 0 0.2rem #30da2a4a"
-        }
-    }
-};
-
-function NewCustomer({ classes, show, onHide }) {
-    const [fullName, setFullName] = useState();
-    const [email, setEmail] = useState();
-    const [pc, setPC] = useState();
-
-    const [pcItems, setPCItems] = useState();
+function NewCustomer({ show, onHide }) {
     const [isSendingData, setSendingData] = useState(false);
 
-    const fetchPC = () => {
-        axios
-            .get(ApiRoutes.pc)
-            .then(({ data }) => {
-                setPCItems(data);
-                setPC(data[0].id);
-            })
-            .catch(e => console.log(e));
-    };
-
-    useEffect(() => {
-        fetchPC();
-    }, []);
-
-    const inputChanged = ({ target }) => {
-        switch (target.name) {
-            case "full_name":
-                setFullName(target.value);
-                break;
-            case "email":
-                setEmail(target.value);
-                break;
-            case "pc":
-                setPC(target.value);
-                break;
-        }
-    };
-
-    const formSubmitted = e => {
-        e.preventDefault();
-
-        const newCustomer = {
-            full_name: fullName,
-            email: email,
-            pc: pc
-        };
-
+    const formSubmitted = newCustomer => {
         setSendingData(true);
 
         axios
@@ -82,72 +20,8 @@ function NewCustomer({ classes, show, onHide }) {
 
     return (
         <CardModal show={show} onHide={onHide} title="Новый пользователь">
-            {pcItems && !isSendingData ? (
-                <form onSubmit={formSubmitted}>
-                    <div className="form-group">
-                        <label htmlFor="full_name">Полное имя</label>
-                        <input
-                            type="string"
-                            className="form-control"
-                            id="full_name"
-                            name="full_name"
-                            value={fullName}
-                            onChange={inputChanged}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            id="email"
-                            name="email"
-                            value={email}
-                            onChange={inputChanged}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="pc">ПК</label>
-                        <select
-                            className="form-control"
-                            id="pc"
-                            name="pc"
-                            value={pc}
-                            onChange={inputChanged}
-                        >
-                            {pcItems.map((pcItem, i) => (
-                                <option key={i} value={pcItem.id}>
-                                    {pcItem.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="row justify-content-end">
-                        <div className="col-7">
-                            <div className="row">
-                                <div className="col-6 mb-2 mb-xl-0">
-                                    <button
-                                        className={
-                                            "btn btn-primary " +
-                                            classes.actionButton
-                                        }
-                                        onClick={onHide}
-                                    >
-                                        Отмена
-                                    </button>
-                                </div>
-                                <div className="col-6">
-                                    <button
-                                        className={`btn ${classes.actionButton} ${classes.acceptActionButton}`}
-                                        type="submit"
-                                    >
-                                        Сохранить
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+            {!isSendingData ? (
+                <CustomerForm onSubmit={formSubmitted} onCancel={onHide} />
             ) : (
                 <Loading />
             )}
@@ -155,4 +29,4 @@ function NewCustomer({ classes, show, onHide }) {
     );
 }
 
-export default withStyles(styles)(NewCustomer);
+export default NewCustomer;
