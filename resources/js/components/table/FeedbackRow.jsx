ed0e6e710e2b768b31862fc8be2aecd1594729e6
Ergashev Akbar <ergashev.akb@gmail.com>
2019-12-27 18:10:07 +0500

    Formate

--- resources/js/components/table/FeedbackRow.jsx
+++ resources/js/components/table/FeedbackRow.jsx
@@ -1,97 +1,94 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import withStyles from 'react-jss'
+import React from "react";
+import PropTypes from "prop-types";
+import withStyles from "react-jss";
 import { withRouter } from "react-router-dom";
 
 const styles = {
     tableRow: {
-        cursor: 'pointer',
-        '&:hover': {
-            background: '#f5f5f5'
-        },
+        cursor: "pointer",
+        "&:hover": {
+            background: "#f5f5f5"
+        }
     },
     description: {
-        color: '#3C95D1',
-        width: '35em',
+        color: "#3C95D1",
+        width: "35em"
     },
     created_at: {
-        width: '15em',
-        paddingRight: '8rem !important',
+        width: "15em",
+        paddingRight: "8rem !important"
     },
     logo: {
-        width: '5em',
+        width: "5em"
     },
     statusWrapper: {
-        textAlign: 'center !important',
-        paddingRight: '5rem !important',
-        width: '17em !important'
+        textAlign: "center !important",
+        paddingRight: "5rem !important",
+        width: "17em !important"
     },
     status: {
-        background: '#4599f3',
-        color: 'white',
-        padding: '.5rem',
-        borderRadius: '50px'
-    },
-}
+        background: "#4599f3",
+        color: "white",
+        padding: ".5rem",
+        borderRadius: "50px"
+    }
+};
 
 function FeedbackRow({ classes, feedback, printableFields, history }) {
-    const shoudPrint = (field) => printableFields.includes(field);
+    const shoudPrint = field => printableFields.includes(field);
 
     const onFeedbackClick = () => {
-        history.push('/feedbacks/' + feedback.id);
-    }
+        history.push("/feedbacks/" + feedback.id);
+    };
+
+    const emptyValue = ( <td>Данные были удалены</td> )
+
+    const { description, created_at, customer, status } = feedback;
 
     return (
         <tr className={classes.tableRow} onClick={onFeedbackClick}>
-            {
-                shoudPrint("description")
-                    ? (
-                        <td className={classes.description}>
-                            {
-                                feedback.description.length > 60
-                                    ? feedback.description.slice(0, 60) + '...'
-                                    : feedback.description
-                            }
-                        </td>
-                    )
-                    : null
-            }
-            {
-                shoudPrint("created_at")
-                    ? (<td className={classes.created_at}>{feedback.created_at}</td>)
-                    : null
-            }
-            {
-                shoudPrint("customer.pc")
-                    ? (
-                        <td>
-                            <img src={feedback.customer.pc.logo} alt={feedback.customer.pc.name} className={classes.logo} />
-                        </td>
-                    )
-                    : null
-            }
-            {
-                shoudPrint("customer.user.full_name")
-                    ? (<td>{feedback.customer.user.full_name}</td>)
-                    : null
-            }
-            {
-                shoudPrint("status.name")
-                    ? (
-                        <td className={classes.statusWrapper}>
-                            <div className={classes.status} style={{ background: feedback.status.color }}>
-                                {feedback.status.name}
-                            </div>
-                        </td>
-                    )
-                    : null
-            }
+            {shoudPrint("description") ? (
+                <td className={classes.description}>
+                    {description.length > 60
+                        ? description.slice(0, 60) + "..."
+                        : description}
+                </td>
+            ) : null}
+            {shoudPrint("created_at") ? (
+                <td className={classes.created_at}>{created_at}</td>
+            ) : null}
+            {shoudPrint("customer.pc") ? (
+                <td>
+                    {customer ? (
+                        <img
+                            src={customer.pc.logo}
+                            alt={customer.pc.name}
+                            className={classes.logo}
+                        />
+                    ) : emptyValue}
+                </td>
+            ) : null}
+            {shoudPrint("customer.user.full_name") ? (
+                <td>{customer ? customer.user.full_name : emptyValue}</td>
+            ) : null}
+            {shoudPrint("status.name") ? (
+                <td className={classes.statusWrapper}>
+                    {status ? (
+                        <div
+                            className={classes.status}
+                            style={{ background: status.color }}
+                        >
+                            {status.name}
+                        </div>
+                    ) : emptyValue}
+                </td>
+            ) : null}
         </tr>
-    )
+    );
 }
 
 FeedbackRow.propTypes = {
-    feedback: PropTypes.object.isRequired,
-}
+    feedback: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(withRouter(FeedbackRow));
