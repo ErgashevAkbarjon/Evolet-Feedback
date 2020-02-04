e0409650f8e6bb40d3a62cef154c4a4840325fb3
Ergashev Akbar <ergashev.akb@gmail.com>
2020-02-04 17:39:16 +0500

    Change styling

--- resources/js/components/ManualLink.jsx
+++ resources/js/components/ManualLink.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function ManualLink() {
     const onButtonClick = () => {
         axios({
-            url: '/api/manual',
-            method: 'GET',
-            responseType: 'blob'
-          }).then((response) => {
+            url: "/api/manual",
+            method: "GET",
+            responseType: "blob"
+        }).then(response => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
+            const link = document.createElement("a");
             link.href = url;
-            link.setAttribute('download', 'Evolet-Feedback-Manual.docx');
+            link.setAttribute("download", "Evolet-Feedback-Manual.docx");
             link.click();
-          });
+        });
     };
 
     return (
-            <button className="btn btn-link" onClick={onButtonClick}>
-                Инструкция
-            </button>
+        <button
+            className="btn btn-link text-decoration-none"
+            onClick={onButtonClick}
+        >
+            Инструкция
+        </button>
     );
 }
 export default ManualLink;
