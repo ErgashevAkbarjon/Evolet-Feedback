2c58977dc26b1296f307740f2e8206b9453b7edb
Ergashev Akbar <ergashev.akb@gmail.com>
2020-03-16 10:18:23 +0500

    Refactor tables

--- resources/js/components/table/Table.jsx
+++ resources/js/components/table/Table.jsx
@@ -1,38 +1,116 @@
-import React from 'react';
-import withStyles from 'react-jss';
+import React, { useState, useEffect } from "react";
+import withStyles from "react-jss";
+import PropTypes from "prop-types";
+import Loading from "../Loading";
 
 const styles = {
     tableWrapper: {
         border: "1px solid #cccccc",
         borderRadius: "5px",
         overflow: "hidden",
-        marginBottom: '2rem',
-        '& table': {
-            marginBottom: '0px',
-            color: '#707070',
+        marginBottom: "2rem",
+        "& table": {
+            marginBottom: "0px",
+            color: "#707070"
         },
-        '& th': {
-            borderBottom: '1px solid #cccccc !important',
-            borderTop: 'unset',
-            background: '#F5F5F5',
-            fontWeight: '400',
+        "& th": {
+            borderBottom: "1px solid #cccccc !important",
+            borderTop: "unset",
+            background: "#F5F5F5",
+            fontWeight: "400"
         },
-        '& td': {
-            border: 'none',
-            verticalAlign: "middle",
+        "& td": {
+            border: "none",
+            verticalAlign: "middle"
         }
-    },
+    }
 };
 
+function Table({ classes, items, onPrintRow, headers, onSortBy }) {
+    const [headerToSort, setHeaderToSort] = useState(null);
 
-function Table({classes, children}) {
+    const printRow = (item, i) => {
+        if(onPrintRow){
+            return onPrintRow(item, i);
+        }
+
+        return (
+            <tr>
+                
+            </tr>
+        );
+    };
+
+    const onHeaderClick = header => {
+        if (!onSortBy) return;
+
+        const newHeaderToSort = headerToSort && headerToSort.header !== header;
+
+        if (!headerToSort || newHeaderToSort) {
+            setHeaderToSort({
+                header,
+                isDesc: false
+            });
+        } else {
+            setHeaderToSort({ header, isDesc: !headerToSort.isDesc });
+        }
+    };
+
+    const onHeaderToSortChange = () => {
+        if (!headerToSort) return;
+
+        const { header, isDesc } = headerToSort;
+
+        onSortBy(header, isDesc);
+    };
+
+    useEffect(onHeaderToSortChange, [headerToSort]);
+
+    const getHeaderString = header => {
+        const sortingByCurrentHeader =
+            headerToSort && headerToSort.header === header;
+
+        if (sortingByCurrentHeader) {
+            return headerToSort.isDesc ? `${header} sd` : `${header} s`;
+        }
+
+        return header;
+    };
+
     return (
         <div className={classes.tableWrapper}>
             <table className="table">
-                {children}
+                <thead>
+                    <tr>
+                        {headers.map((header, i) => (
+                            <th
+                                className="unselectable-text"
+                                key={i}
+                                onClick={() => onHeaderClick(header)}
+                            >
+                                {getHeaderString(header)}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {items ? (
+                        items.map(printRow)
+                    ) : (
+                        <tr>
+                            <td colSpan={headers.length}>
+                                <Loading />
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
             </table>
         </div>
-    )
+    );
 }
 
-export default withStyles(styles)(Table)
+Table.propTypes = {
+    headers: PropTypes.array.isRequired
+};
+
+export default withStyles(styles)(Table);
