f6cd0b8cc231e357b4b04e23e9ec95b0d7c0d4e0
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-07 14:33:18 +0500

    Add pagination to employees

--- resources/js/views/employee/employees/Employees.jsx
+++ resources/js/views/employee/employees/Employees.jsx
@@ -15,21 +15,22 @@ const printableFields = [
     {
         name: "avatar",
         label: "Аватар",
-        sortable: false
+        sortable: false,
     },
     {
         name: "user.full_name",
-        label: "Имя"
+        label: "Имя",
     },
     {
         name: "user.email",
-        label: "Почта"
-    }
+        label: "Почта",
+    },
 ];
 
 function Employees({ match }) {
     const [employees, setEmployees] = useState(null);
-    const [employeesUrl, setEmployeesUrl] = useState(ApiRoutes.employees);
+    const [sortQuery, setSortQuery] = useState("");
+    const [paginationQuery, setPaginationQuery] = useState("");
 
     const [selectedEmployee, setSelectedEmployee] = useState(null);
 
@@ -38,12 +39,12 @@ function Employees({ match }) {
     const [employeeToEdit, setEmployeeToEdit] = useState(null);
     const [employeeToDelete, setEmployeeToDelete] = useState(null);
 
-    const fetchEmployee = id => {
+    const fetchEmployee = (id) => {
         const employeeURL = ApiRoutes.employees + "/" + id;
         axios
             .get(employeeURL)
             .then(({ data }) => setSelectedEmployee(data))
-            .catch(e => console.log(e));
+            .catch((e) => console.log(e));
     };
 
     useEffect(() => {
@@ -53,22 +54,24 @@ function Employees({ match }) {
     }, [match]);
 
     const fetchEmployees = () => {
+        const employeesUrl = ApiRoutes.employees + "?" + sortQuery + paginationQuery;
+
         axios
             .get(employeesUrl)
-            .then(({ data }) => setEmployees(data))
-            .catch(e => console.log(e));
+            .then(({ data, pagination }) => setEmployees({ data, pagination }))
+            .catch((e) => console.log(e));
     };
 
     useEffect(() => {
         updateEmployeeList();
-    }, [employeesUrl]);
+    }, [sortQuery, paginationQuery]);
 
     const updateEmployeeList = () => {
         setEmployees(null);
         fetchEmployees();
     };
 
-    const employeeSelected = employee => {
+    const employeeSelected = (employee) => {
         setSelectedEmployee(employee);
     };
 
@@ -78,34 +81,46 @@ function Employees({ match }) {
         updateEmployeeList();
     };
 
-    const onEmployeeEdit = employee => {
+    const onEmployeeEdit = (employee) => {
         setSelectedEmployee(null);
         setEmployeeToEdit(employee);
     };
 
-    const onEmployeeDelete = employee => {
+    const onEmployeeDelete = (employee) => {
         setSelectedEmployee(null);
         setEmployeeToDelete(employee);
     };
 
-    const onEmployeeUpdated = employee => {
+    const onEmployeeUpdated = (employee) => {
         setEmployeeToEdit(null);
         updateEmployeeList();
 
         setSelectedEmployee(employee);
     };
 
-    const onEmployeeDeleted = employee => {
+    const onEmployeeDeleted = (employee) => {
         setEmployeeToDelete(null);
 
         updateEmployeeList();
     };
 
-    const onSortEmployees = sortQuery => {
+    const onSortEmployees = (sortQuery) => {
         if (!sortQuery) return;
 
-        setEmployeesUrl(ApiRoutes.employees + "?" + sortQuery);
+        setSortQuery(sortQuery);
     };
+
+    const onEmployeesPageChange = (page, perPage) => {
+        setPaginationQuery(`&page=${page}&perPage=${perPage}`);
+    };
+
+    let employeesList = null;
+    let employeesPagination = null;
+
+    if (employees) {
+        employeesList = employees.data;
+        employeesPagination = employees.pagination;
+    }
 
     return (
         <div>
@@ -120,8 +135,10 @@ function Employees({ match }) {
                 </div>
             </TableTitle>
             <Table
-                items={employees}
+                items={employeesList}
                 headers={printableFields}
+                paginationData={employeesPagination}
+                onPageChange={onEmployeesPageChange}
                 onSort={onSortEmployees}
                 onPrintRow={(employee, i) => (
                     <EmployeeRow
