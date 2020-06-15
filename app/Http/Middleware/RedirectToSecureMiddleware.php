fb48135a3ac7b22bf69b753607ba9e45ae24482e
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-15 10:22:22 +0500

    Add middleware to force redirect http to https

---
+++ app/Http/Middleware/RedirectToSecureMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RedirectToSecureMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if( !$request->secure() && app()->environment('production')){
+            return redirect($request->getRequestUri(), 301, [], true);
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,9 +57,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\RedirectToSecureMiddleware::class
+]);
 
 $app->routeMiddleware([
     'jwt.auth' => App\Http\Middleware\JWTMiddleware::class,
